b6f6de3178f1c523367d8394acf8b0dbdf2ab262
Ethan Walker <ethan.walker830@gmail.com>
2026-05-19 20:18:41 -0700

    better checking for valid entries in sudoku solver

--- sudoku/solver.go
+++ sudoku/solver.go
@@ -5,37 +5,6 @@ import (
 	"time"
 )
 
-func checkValidEntry(board [][]byte, i int, j int) bool {
-	if board[i][j] == '.' {
-		return false
-	}
-	// check row
-	if valid := sectionIsValid(board[i]); !valid {
-		return false
-	}
-
-	// check col
-	col := make([]byte, 9)
-	for k := range 9 {
-		col[k] = board[k][j]
-	}
-	if valid := sectionIsValid(col); !valid {
-		return false
-	}
-
-	// check square
-	r, c := (i/3)*3, (j/3)*3
-	for k := range 3 {
-		for l := range 3 {
-			col[k*3+l] = board[r+k][c+l]
-		}
-	}
-	if valid := sectionIsValid(col); !valid {
-		return false
-	}
-	return true
-}
-
 func getIJ(v int) (int, int) {
 	j := v % 9
 	i := v / 9
@@ -65,8 +34,29 @@ func copyBoard(board [][]byte) [][]byte {
 	return suggestion
 }
 
+func isNumUsed(num byte, i int, j int, row [9][10]bool, col [9][10]bool, box [9][10]bool) bool {
+	digit := int(num - '0')
+	return !row[i][digit] && !col[j][digit] && !box[(j/3)+(i/3)*3][digit]
+}
+
 func solveSudoku(board [][]byte) {
 	checker := copyBoard(board)
+
+	row := [9][10]bool{}
+	col := [9][10]bool{}
+	box := [9][10]bool{}
+
+	for i := range 9 {
+		for j := range 9 {
+			if board[i][j] != '.' {
+				digit := int(board[i][j] - '0')
+				row[i][digit] = true
+				col[j][digit] = true
+				box[(j/3)+(i/3)*3][digit] = true
+			}
+		}
+	}
+
 	v := 0
 	for v < 81 {
 		i, j := getIJ(v)
@@ -75,6 +65,10 @@ func solveSudoku(board [][]byte) {
 			case '.':
 				board[i][j] = '1'
 			case '9':
+				digit := int(board[i][j] - '0')
+				row[i][digit] = false
+				col[j][digit] = false
+				box[(j/3)+(i/3)*3][digit] = false
 				board[i][j] = '.'
 				v -= 1
 				i, j = getIJ(v)
@@ -84,13 +78,21 @@ func solveSudoku(board [][]byte) {
 				}
 				continue
 			default:
+				digit := int(board[i][j] - '0')
+				row[i][digit] = false
+				col[j][digit] = false
+				box[(j/3)+(i/3)*3][digit] = false
 				board[i][j] += 1
 			}
 
 			isValid := false
 			for board[i][j] <= '9' {
-				isValid = checkValidEntry(board, i, j)
+				isValid = isNumUsed(board[i][j], i, j, row, col, box)
 				if isValid {
+					digit := int(board[i][j] - '0')
+					row[i][digit] = true
+					col[j][digit] = true
+					box[(j/3)+(i/3)*3][digit] = true
 					break
 				}
 				board[i][j] += 1
